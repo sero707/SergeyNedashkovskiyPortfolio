a156d0252587008e3243f42539c381747d823b85
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2022-09-17 02:44:59 +0300

    hamburger active

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,11 @@
 import * as flsFunction from "./modules/functions.js";
 flsFunction.isWebp();
+
+const hamburger = document.querySelector('.hamburger'),
+    menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('menu_active');
+    hamburger.classList.toggle('hamburger_active');
+})
+
