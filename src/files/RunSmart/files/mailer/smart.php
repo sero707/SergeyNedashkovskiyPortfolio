1607fd2383c42054086e2fdfade1c777ce9b6cf3
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2022-09-22 22:22:44 +0300

    add portfolio

---
+++ src/files/RunSmart/files/mailer/smart.php
@@ -0,0 +1,50 @@
+<?php 
+
+$name = $_POST['name'];
+$phone = $_POST['telephone'];
+$email = $_POST['email'];
+$order = $_POST['order-des'];
+$formName = $_POST['form-name'];
+
+
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'call.back.1@mail.ru';                 			// Наш логин
+$mail->Password = 'GMuuv0qudTAe8JMhQWxj';                           // Наш пароль от ящика
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
+ 
+$mail->setFrom('call.back.1@mail.ru', 'RunSmart');   // От кого письмо 
+$mail->addAddress('sero70707@gmail.com');     // Add a recipient
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Данные';
+$mail->Body    = '
+		Пользователь оставил данные <br>
+	Тип заявки: '. $formName .' <br>
+	Товар: '. $order .' <br>
+	Имя: ' . $name . ' <br>
+	Номер телефона: ' . $phone . '<br>
+	E-mail: ' . $email . '';
+
+if(!$mail->send()) {
+    return false;
+} else {
+    return true;
+}
+
+?>
