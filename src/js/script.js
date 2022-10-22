9981a476b17f54271fa0c852ad1b8b3e2906dad5
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2022-10-23 00:05:55 +0300

    fixing

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,34 @@
 import * as flsFunction from "./modules/functions.js";
 flsFunction.isWebp();
+(function() {
+    'use strict';
+   
+    
 
-const hamburger = document.querySelector('.hamburger'),
-    menu = document.querySelector('.menu');
+    const hamburger = document.querySelector('.hamburger'),
+          menu = document.querySelector('.menu');
 
-hamburger.addEventListener('click', () => {
-    menu.classList.toggle('menu_active');
-    hamburger.classList.toggle('hamburger_active');
-})
+    hamburger.addEventListener('click', () => {
+        menu.classList.toggle('menu_active');
+        hamburger.classList.toggle('hamburger_active');
+    });
+    function closeMenuList (){
+        menu.addEventListener('click', (e) => {
+            if (e.target === menu){
+                menu.classList.remove('menu_active');
+                hamburger.classList.remove('hamburger_active');
+            }
+        });
+        document.addEventListener('keydown', (e) => {
+            if(e.code === 'Escape' && menu.classList.contains('menu_active')){
+                menu.classList.remove('menu_active');
+                hamburger.classList.remove('hamburger_active');
+            }
+        });
+    }
+    closeMenuList();
+  })();
+
+
+
 
